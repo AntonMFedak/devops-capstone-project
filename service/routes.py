4da8d729d9fce1bdde615873db8e989aebdaaf84
Anton Marques Fedak <97338191+AntonMFedak@users.noreply.github.com>
2025-05-12 13:09:47 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -50,10 +50,9 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
-"""
-LIST ALL ACCOUNTS
-"""
-
+######################################################################
+# LIST ALL ACCOUNTS
+######################################################################
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     app.logger.info("Request to list Accounts")
